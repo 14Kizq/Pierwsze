06ebcfe8212a5bc633b241f2abae783e8ccbc4fe
14Kizq <Kizq@wp.pl>
2019-03-09 16:37:01 +0100

    odcinek szósty

---
+++ kurs6.py
@@ -0,0 +1,13 @@
+
+person = {"wiek": 20, "imie": "Ania", "nazwisko": "Kowalska"}
+
+keys = person.keys()
+
+print(keys)
+
+print(person["nazwisko"])
+print(person["imie"])
+print(person.get("wzrost ",25))
+
+print(type(person))
+
